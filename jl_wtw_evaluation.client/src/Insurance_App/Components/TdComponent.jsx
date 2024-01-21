fa32b982b214d654d040c8f7877b1b0f7e9581c9
Jondragon77221155 <jondragon_m@yahoo.com.mx>
2024-01-21 13:36:30 -0600

    Adjustment

--- jl_wtw_evaluation.client/src/Insurance_App/Components/TdComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function TdComponent(props) {
-  return (
-      <td>{props}</td>
-  );
-}
-
-export default TdComponent;
--- jl_wtw_evaluation.client/src/Insurance_App/Main.jsx
+++ jl_wtw_evaluation.client/src/Insurance_App/Main.jsx
@@ -1,22 +1,155 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDom from "react-dom";
-import CalculateButtonComponent from "./Components/CalculateButtonComponent.jsx";
-import InputNumberComponent from "./Components/InputNumberComponent.jsx";
-import TypeExecutionComponent from "./Components/TypeExecutionComponent.jsx";
-import TableComponent from "./Components/TableComponent.jsx";
+//import CalculateButtonComponent from "./Components/CalculateButtonComponent.jsx";
+//import InputNumberComponent from "./Components/InputNumberComponent.jsx";
+//import TypeExecutionComponent from "./Components/TypeExecutionComponent.jsx";
+//import TableComponent from "./Components/TableComponent.jsx";
 
 import "./Style/IsuranceStyle.css";
 
 function FormC() {
-    return (<div className="container">
-        <h1>Welcome</h1>
-        <InputNumberComponent labelText={"Main Limit: "} />
-        <InputNumberComponent labelText={"Main Retention: "} />
-        <TypeExecutionComponent labelText={"Type of Execution: "} />
-        <CalculateButtonComponent />
-        <TableComponent />
+    const [inputsCollection, setInputsCollection] = useState({
+        mainLimit: 0,
+        mainRetention: 0,
+        fecha: getDate()
+    });
+    const [typeInput, setTypeInput] = useState("")
+    const options = [
+        { opt: 'Select an Option', value: 0 },
+        { opt: 'Simple', value: 1 },
+        { opt: 'Complex', value: 2 }];
 
-    </div>);
+    const [items, setItems] = useState([]);
+
+    const [currentDate, setCurrentDate] = useState(getDate());
+    const [showDelayedText, setShowDelayedText] = useState(false);
+
+    const delay = async (ms) => {
+        return new Promise((resolve) =>
+            setTimeout(resolve, ms));
+    };
+
+    const handleOnClick = async () => {
+        setShowDelayedText(true)
+        await delay(3000);
+        setShowDelayedText(false);
+        console.log(showDelayedText);
+        addItem();
+    };
+    function getDate() {
+        const today = new Date();
+        var month = "";
+        const year = today.getFullYear();
+        const date = today.getDate();
+        if ((today.getMonth() + 1) === 1) {
+            month = "Jan";
+        } else if ((today.getMonth() + 1) === 2) {
+            month = "Feb";
+        } else if ((today.getMonth() + 1) === 3) {
+            month = "Mar";
+        } else if ((today.getMonth() + 1) === 4) {
+            month = "Apr";
+        } else if ((today.getMonth() + 1) === 5) {
+            month = "May";
+        } else if ((today.getMonth() + 1) === 6) {
+            month = "Jun";
+        } else if ((today.getMonth() + 1) === 7) {
+            month = "Jul";
+        } else if ((today.getMonth() + 1) === 8) {
+            month = "Agu";
+        } else if ((today.getMonth() + 1) === 9) {
+            month = "Sep";
+        } else if ((today.getMonth() + 1) === 10) {
+            month = "Oct";
+        } else if ((today.getMonth() + 1) === 11) {
+            month = "Nov";
+        } else if ((today.getMonth() + 1) === 12) {
+            month = "Dec";
+        }
+
+        //setCurrentDate(`${date} ${month} ${year}`);
+        return `${date} ${month} ${year}`;
+    }
+
+    function handelSelect(event) {
+        const selectedVal = event.target.value;
+        setTypeInput(selectedVal);
+
+    }
+    function handleChange(event) {
+        const { name, value } = event.target;
+
+        setInputsCollection(prevValue => {
+            if (name === "mainLimit") {
+                return {
+                    mainLimit: value,
+                    mainRetention: prevValue.mainRetention,
+                    fecha: prevValue.fecha
+                };
+            } else if (name === "mainRetention") {
+                return {
+                    mainLimit: prevValue.mainLimit,
+                    mainRetention: value,
+                    fecha: prevValue.fecha
+                };
+            } else if (name === "fecha") {
+                return {
+                    mainLimit: prevValue.mainLimit,
+                    mainRetention: prevValue.mainRetention,
+                    fecha: value
+                };
+            }
+        });
+    }
+
+    function addItem() {
+        setItems(prevItems => {
+            //return [...prevItems, inputText];
+            return [...prevItems, inputsCollection];
+        });
+        setInputsCollection({ mainLimit: 0, mainRetention: 0 });
+        setTypeInput( `${options[0]}`);
+    }
+
+    return (
+        <div className="container">
+            <div className="heading">
+                <h1>Welcome</h1>
+            </div>
+            <div className="form">
+                <label>Main Limit:</label>
+                <input name="mainLimit" onChange={handleChange} type="number" value={inputsCollection.mainLimit} />
+                <label>Retain Limit:</label>
+                <input name="mainRetention" onChange={handleChange} type="number" value={inputsCollection.mainRetention} />
+                <label>Type:</label>
+                <select name="fecha" onChange={handelSelect} value={typeInput}>
+                    {options.map((option, index) => (
+                        <option key={index} value={option.value} >{option.opt}</option>
+                    ))}
+                </select>
+                <button onClick={handleOnClick}>
+                    <span>Calculate</span>
+                </button>
+                <label id="loadingLabel" style={showDelayedText ? { visibility: "visible" } : { visibility: "collapse" }}>Loading...</label>
+            </div>
+            <div>
+                <table>
+                    <tr>
+                        <th>Reference Date</th>
+                        <th>Benchmark 1</th>
+                        <th>Benchmark 2</th>
+                    </tr>
+                    {items.map((element, index) => (
+                        <tr key={index}>
+                            <td>{currentDate}</td>
+                            <td>{`${(element.mainRetention / element.mainLimit) * 100}` + '%'}</td>
+                            <td>${element.mainRetention}</td>
+                        </tr>
+                    ))}
+                </table>
+            </div>
+        </div>
+    );
 }
 
 
